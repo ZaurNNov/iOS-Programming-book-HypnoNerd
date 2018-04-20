d5c4bd261442ff95b2a1a3d6a5597f20bb7b62fe
Zaur Giyasov <zagiyas@mera.ru>
2018-04-20 14:31:03 +0300

    7-done

--- HypnoNerd/HypnosisViewController.m
+++ HypnoNerd/HypnosisViewController.m
@@ -9,12 +9,75 @@
 #import "HypnosisViewController.h"
 #import "HypnosisView.h"
 
+@interface HypnosisViewController () <UITextFieldDelegate>
+
+@end
+
 @implementation HypnosisViewController
 
 -(void)loadView {
     // Create a view
-    HypnosisView *backgroundView = [[HypnosisView alloc]init];
+    CGRect frame = [UIScreen mainScreen].bounds;
+    HypnosisView *backgroundView = [[HypnosisView alloc]initWithFrame:frame];
+    
+    CGRect textFieldRect = CGRectMake(40, 90, 240, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"This is textField plaseholder";
+    textField.returnKeyType = UIReturnKeyDone;
+    textField.keyboardType = UIKeyboardTypeDefault;
+    textField.enablesReturnKeyAutomatically = YES;
+    textField.delegate = self;
+    [backgroundView addSubview:textField];
+    
+    
     self.view = backgroundView;
+}
+
+// TextField delegate methods
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+//    NSLog(@"(BOOL)textFieldShouldReturn:(UITextField *)textField");
+//    NSLog(@"%@", textField.text);
+    [self drawHypnoMess:textField.text];
+    textField.text = @"";
+    [textField resignFirstResponder];
+    return YES;
+}
+
+-(void)drawHypnoMess: (NSString *)mess {
+    for (int i = 0; i < 20; i++) {
+        UILabel *messageLabel = [[UILabel alloc] init];
+        // Config
+        messageLabel.backgroundColor = [UIColor whiteColor];
+        messageLabel.text = mess;
+        
+        //resize
+        [messageLabel sizeToFit];
+        //rand plase
+        int width = (int)(self.view.bounds.size.width - messageLabel.bounds.size.width);
+        int heght = (int)(self.view.bounds.size.height - messageLabel.bounds.size.height);
+        int x = arc4random() % width;
+        int y = arc4random() % heght;
+        
+        //upd frame
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        
+        //add it
+        [self.view addSubview:messageLabel];
+        
+        UIInterpolatingMotionEffect *motionEffect;
+        motionEffect = [[UIInterpolatingMotionEffect alloc]initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        motionEffect.minimumRelativeValue = @(-25);
+        motionEffect.maximumRelativeValue = @(25);
+        [messageLabel addMotionEffect:motionEffect];
+        
+        motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.y" type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+        motionEffect.maximumRelativeValue = @(-25);
+        motionEffect.minimumRelativeValue = @(25);
+        [messageLabel addMotionEffect:motionEffect];
+    }
 }
 
 -(instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
